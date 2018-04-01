2abd16a53f40ecf4971684e66268698553177e28
009shanshukla <shantanushukla61@gmail.com>
2018-04-02 05:04:37 +0530

    text and hard coding

--- init.py
+++ init.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 ### will initialize the screen 
 pygame.init()
@@ -8,34 +9,61 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 
+DisplayWidth = 800
+DisplayHeight = 600
+
 ### will give length and width to the screen
-GameDisplay = pygame.display.set_mode((800, 600))
+GameDisplay = pygame.display.set_mode((DisplayWidth, DisplayHeight))
 
 ### giving title to the scree
 pygame.display.set_caption('Slither')
 
 
 
-
-GameEvent = False
-Game_x = 200
-Game_y = 300
+### Game variables
+GameExit = False
+Game_x = DisplayWidth/2
+Game_y = DisplayHeight/2
 Game_x_change = 0
+Game_y_change = 0
+BlockSize = 10
 
+### clock object initialization
 clock = pygame.time.Clock()
+FPS = 30
+
+### font object initialization using system font
+font = pygame.font.SysFont(None, 25)
+
+### message that needs to be printed on screen
+def message_to_screen(msg, color):
+	ScreenText = font.render(msg, True, color)
+	GameDisplay.blit(ScreenText, [DisplayWidth/2, DisplayHeight/2])
 
 ### looping all events
-while not GameEvent:
+while not GameExit:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			GameEvent = True
+			GameExit = True
 
 		### if an arrow key is pressed		
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_LEFT:
-				Game_x_change = -10
-			if event.key == pygame.K_RIGHT:
-				Game_x_change = 10	 
+				Game_x_change = -BlockSize
+				Game_y_change = 0
+			elif event.key == pygame.K_RIGHT:
+				Game_x_change = BlockSize
+				Game_y_change = 0
+			elif event.key == pygame.K_UP:
+				Game_y_change = -BlockSize
+				Game_x_change = 0
+			elif event.key == pygame.K_DOWN:
+				Game_y_change = BlockSize
+				Game_x_change = 0
+
+		### off the boundary			
+		if Game_x >= DisplayWidth or Game_x < 0 or Game_y >= DisplayHeight or Game_y < 0:
+			GameExit = True			 
 
 		### adding case if user lifts up from the key then movement stops 		
 		#if event.type == pygame.KEYUP:
@@ -44,18 +72,23 @@ while not GameEvent:
 
 
 	### will change according to last arrow key pressed			
-	Game_x += Game_x_change 			
+	Game_x += Game_x_change 
+	Game_y += Game_y_change			
 	GameDisplay.fill(white)	
 
 	### parameter(ScreenObject, color, [x, y, w, h])
-	pygame.draw.rect(GameDisplay, black, [Game_x, Game_y, 10, 100])
+	pygame.draw.rect(GameDisplay, black, [Game_x, Game_y, BlockSize, BlockSize])
 
 	### will update the whole display screen
 	pygame.display.update()
 
 	### give freeze time frame per seconds
-	clock.tick(15)	
+	clock.tick(FPS)	
 
+### final msg on screen for 2 secs
+message_to_screen("You Lose", red)
+pygame.display.update()
+time.sleep(2)
 
 ### quitting from pygame
 pygame.quit()
